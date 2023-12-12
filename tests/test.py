dafd4473926eb6f9bc0dfac5abfee13764fb6e34
lemon571 <91371812+lemon571@users.noreply.github.com>
2023-12-12 12:05:21 +0300

    Add to test.py not completed

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,33 @@
+import pytest
 from framework.icn_page import ICNPage
 
+@pytest.fixture(scope="module")
+def icn_page(driver):
+    page = ICNPage(driver)
+    page.open()
+    page.open_icn()
+    return page
+
+@pytest.mark.parametrize("version, expected_ram_text", [
+    ("4.0", "RAM: 8 GB, and an additional 2 GB for each monitoring point on this computer"),
+    ("3.0", "RAM: 4 GB, and an additional 2 GB for each monitoring point on this computer."),
+    ("4.0", "объем оперативной памяти: 8 ГБ и по 2 ГБ для каждой точки мониторинга на этом компьютере;"),
+    ("3.1", "RAM: 8 GB, and an additional 2 GB for each monitoring point on this computer.")
+])
+
+def test_check_sensor_ram(icn_page, version, expected_ram_text):
+    icn_page.open_version(version)
+    icn_page.open_about()
+    icn_page.open_hardware()
+    ram_text = icn_page.get_ram_text()
+    assert ram_text == expected_ram_text
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    if call.when == "call" and call.excinfo is not None:
+        pytest.exit(f"The test '{item.nodeid}' was not completed")
+
+"""
 def test_check_sensor_ram_v40(driver):
     icn_page = ICNPage(driver)
     icn_page.open()
@@ -39,3 +67,4 @@ def test_check_sensor_ram_v31(driver):
     icn_page.open_hardware()
     ram_text = icn_page.get_ram_text_ru()
     assert ram_text == "RAM: 8 GB, and an additional 2 GB for each monitoring point on this computer."
+"""
